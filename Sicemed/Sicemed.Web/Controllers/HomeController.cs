df46d8aeffd2384aeb4ac4be4732e17a58fa6157
Walter Poch <walter.poch@gmail.com>
2011-02-04 01:23:09 -0300

    Layout armado.
    Signed-off-by: Walter Poch <walter.poch@gmail.com>

--- Sicemed/Sicemed.Web/Controllers/HomeController.cs
+++ Sicemed/Sicemed.Web/Controllers/HomeController.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using Castle.Core.Logging;
 
 namespace Sicemed.Web.Controllers {
     public class HomeController : Controller {
+        public ILogger Logger { get; set; }
+
         public ActionResult Index() {
+            Logger.FatalFormat("ERRORRRRR!!!");
+
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
             return View();
--- Sicemed/Sicemed.Web/Global.asax.cs
+++ Sicemed/Sicemed.Web/Global.asax.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Castle.Core.Logging;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using Sicemed.Web.Plumbing;
 
 namespace Sicemed.Web {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
+    public class MvcApplication : HttpApplication {
+        private static IWindsorContainer _container;
+        private static ILogger _logger;
 
-    public class MvcApplication : System.Web.HttpApplication {
+        public static IWindsorContainer Container { get { return _container; } }
+        public static ILogger Logger { get { return _logger; } }
+
         public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
             filters.Add(new HandleErrorAttribute());
         }
@@ -30,6 +37,27 @@ namespace Sicemed.Web {
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
+
+            //Windsor
+            BootstrapContainer();
+        }
+
+        protected void Application_End() {
+            Container.Dispose();
+        }
+
+        protected void Application_Error(object sender, EventArgs args) {
+            if (Logger.IsFatalEnabled)
+                _logger.FatalFormat("An unhandled error happened. Exc: {0}", Server.GetLastError());
+        }
+
+        private static void BootstrapContainer() {
+            _container = new WindsorContainer()
+                .Install(FromAssembly.This());
+            var controllerFactory = new WindsorControllerFactory(_container.Kernel);
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+
+            _logger = _container.Resolve<ILogger>();
         }
     }
 }
