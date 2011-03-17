f13ac499aeb3f0efa3c1b141422826f85fb0157d
wpoch <walter.poch@gmail.com>
2011-03-17 12:48:40 -0300

    Assets

--- Sicemed/Sicemed.Web/ActionResults/NewtonJsonResult.cs
+++ Sicemed/Sicemed.Web/ActionResults/NewtonJsonResult.cs
@@ -29,7 +29,13 @@ namespace Sicemed.Web.ActionResults
                 throw new ArgumentNullException("context");
             }
 
-            HttpResponseBase response = context.HttpContext.Response;
+            if(JsonRequestBehavior == JsonRequestBehavior.DenyGet &&
+                String.Equals(context.HttpContext.Request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new InvalidOperationException("This request has been blocked because sensitive information could be disclosed to third party web sites when this is used in a GET request. To allow GET requests, set JsonRequestBehavior to AllowGet.");
+            }
+
+            var response = context.HttpContext.Response;
 
             if (!String.IsNullOrEmpty(ContentType))
             {
