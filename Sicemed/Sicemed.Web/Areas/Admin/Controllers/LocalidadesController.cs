b4ebd3d228a85a5d7dda17361f6d5ac50c29c161
wpoch <walter.poch@gmail.com>
2011-09-09 18:00:04 -0300

    Localidades sin andar.

--- Sicemed/Sicemed.Web/Areas/Admin/Controllers/LocalidadesController.cs
+++ Sicemed/Sicemed.Web/Areas/Admin/Controllers/LocalidadesController.cs
@@ -1,14 +1,48 @@
 ﻿using System;
+using System.Linq.Expressions;
 using System.Web.Mvc;
+using Sicemed.Web.Infrastructure;
 using Sicemed.Web.Infrastructure.Controllers;
+using Sicemed.Web.Infrastructure.Exceptions;
+using Sicemed.Web.Models;
 
 namespace Sicemed.Web.Areas.Admin.Controllers
 {
-    public class LocalidadesController : NHibernateController
+    public class LocalidadesController : CrudBaseController<Localidad>
     {
-        public ActionResult Index()
+        #region Overrides of CrudBaseController<Localidad>
+
+        protected override Expression<Func<Localidad, object>> DefaultOrderBy
         {
-            throw new NotImplementedException();
-        }         
+            get { return x => x.Nombre; }
+        }
+
+        public override ActionResult Index()
+        {
+            return View(SessionFactory.GetCurrentSession().QueryOver<Provincia>().OrderBy(x => x.Nombre).Asc.Future());
+        }
+
+        public override JsonResult Nuevo(string oper, Localidad modelo, int paginaId = 0)
+        {
+            if (!oper.Equals("add", StringComparison.InvariantCultureIgnoreCase)) throw new ValidationErrorException();
+            var provinciaIdValue = this.ValueProvider.GetValue("provinciaId");
+            if(provinciaIdValue == null) throw new ValidationErrorException("Debe seleccionar una Provincia para la Localidad.");
+            if(string.IsNullOrWhiteSpace(provinciaIdValue.AttemptedValue)) throw new ValidationErrorException("Debe seleccionar una Provincia para la Localidad.");
+            long provinciaId = 0;
+            long.TryParse(provinciaIdValue.AttemptedValue, out provinciaId);
+            if(provinciaId == 0) throw new ValidationErrorException("Debe seleccionar una Provincia para la Localidad.");
+
+            var session = SessionFactory.GetCurrentSession();
+
+            var provincia = session.Get<Provincia>(provinciaId);
+
+            if (provincia == null)
+                throw new ValidationErrorException("Debe seleccionar una Provincia para la Localidad.");            
+
+            provincia.AgregarLocalidad(modelo);
+
+            return Json(ResponseMessage.Success());
+        }
+        #endregion
     }
 }
---
+++ Sicemed/Sicemed.Web/Infrastructure/Attributes/Filters/PermanentCacheAttribute.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sicemed.Web.Infrastructure.Attributes.Filters {
+    public class PermanentCacheAttribute : ActionFilterAttribute {
+        public override void OnResultExecuted(ResultExecutedContext filterContext) {
+            if (filterContext.Exception == null) {
+                var cache = filterContext.HttpContext.Response.Cache;                
+                cache.SetMaxAge(TimeSpan.FromDays(30));
+                cache.SetLastModified(DateTime.Now);
+                cache.SetExpires(DateTime.Now.AddDays(30)); // For HTTP 1.0 browsers
+                cache.SetValidUntilExpires(true);
+                cache.SetCacheability(HttpCacheability.Public);
+                cache.SetRevalidation(HttpCacheRevalidation.None);
+                cache.VaryByHeaders["Accept-Encoding"] = true; // Tell proxy to cache different versions depending on Accept-Encoding
+            }
+            base.OnResultExecuted(filterContext);
+        }
+    }
+}
