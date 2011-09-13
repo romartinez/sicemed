0d12f04bd9c0a9930f05154286d9a246938238b4
Walter Poch <walter.poch@gmail.com>
2011-09-13 16:04:16 -0300

    Acomodado de usings.

--- SICEMED/SICEMED.Web/Infrastructure/Helpers/SessionExtensions.cs
+++ SICEMED/SICEMED.Web/Infrastructure/Helpers/SessionExtensions.cs
@@ -1,4 +1,7 @@
+using System.Data.SqlClient;
 using NHibernate;
+using NHibernate.Exceptions;
+using Sicemed.Web.Infrastructure.Exceptions;
 
 namespace Sicemed.Web.Infrastructure.Helpers
 {
@@ -6,11 +9,26 @@ namespace Sicemed.Web.Infrastructure.Helpers
     {
         public static void Delete<TEntity>(this ISession session, object id)
         {
-            var queryString = string.Format("delete {0} where id = :id",
-                                            typeof(TEntity));
-            session.CreateQuery(queryString)
-                   .SetParameter("id", id)
-                   .ExecuteUpdate();
+            try
+            {
+                var queryString = string.Format("delete {0} where id = :id",
+                                                typeof(TEntity));
+                session.CreateQuery(queryString)
+                       .SetParameter("id", id)
+                       .ExecuteUpdate();
+                
+            }
+            catch(GenericADOException ex)
+            {
+                if (ex.InnerException != null 
+                    && ex.InnerException is SqlException 
+                    && ex.InnerException.Data["HelpLink.EvtID"] != null 
+                    && ex.InnerException.Data["HelpLink.EvtID"].ToString() == "547")
+                {
+                    throw new ValidationErrorException("No se puede eliminar la entidad ya que hay otras entidades asociadas a esta. Elimine las demás entidades y luego proceda a eliminarla.");
+                }
+                throw;
+            }
         }
     }
 }
