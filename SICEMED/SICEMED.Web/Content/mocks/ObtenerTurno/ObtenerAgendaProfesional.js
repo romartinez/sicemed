3f661aca83bc39f56aff88e74fe0dd1bd8fdc4fd
Walter Poch <walter.poch@gmail.com>
2012-01-18 18:09:20 -0300

    Actualicé KnockoutJS a v2. Agregué la librería para mostrar el calendario con los turnos disponibles.

---
+++ SICEMED/SICEMED.Web/Content/mocks/ObtenerTurno/ObtenerAgendaProfesional.js
@@ -0,0 +1,18 @@
+[
+  {
+    "FechaTurnoInicial": "/new Date()/",
+    "FechaTurnoFinal": "/new Date()/",
+    "Consultorio": {
+      "Id": 1,
+      "Nombre": "PBA 1000"
+    }
+  },
+  {
+    "FechaTurnoInicial": "/new Date()/",
+    "FechaTurnoFinal": "/new Date()/",
+    "Consultorio": {
+      "Id": 2,
+      "Nombre": "PBA 1002"
+    }
+  }
+]
--- SICEMED/SICEMED.Web/Scripts/ObtenerTurno/index.js
+++ SICEMED/SICEMED.Web/Scripts/ObtenerTurno/index.js
@@ -28,8 +28,8 @@ var obtenerTurno = (function () {
     };
 
     // Expone dos eventos:
-    // * reservarTurnoLibre(firmante)
-    // * elegirTurnoEnAgenda(firmante)
+    // * reservarTurnoLibre(profesional)
+    // * elegirTurnoEnAgenda(profesional)
     var seleccionProfesionalesVm = function () {
         var self = this;
         self.especialidades = ko.observableArray([]);
@@ -38,7 +38,7 @@ var obtenerTurno = (function () {
 
         self.profesionalesEncontrados = ko.observableArray([]);
 
-        self.buscarProfesionales = function () {
+        self.buscarProfesionales = function () {            
             self.profesionalesEncontrados.removeAll();
             $.getJSON('/ObtenerTurno/BuscarProfesional').done(function (data) {
                 for (var i = data.length - 1; i >= 0; i--) {
@@ -64,29 +64,78 @@ var obtenerTurno = (function () {
         });
     };
 
+    // Expone un evento
+    // * reservarTurno(turno)
+    var seleccionTurnoVm = function(){
+        var self = this;
+        
+        self.turnosDisponibles = ko.observableArray([]);
+
+        //Incializar agenda
+        $.getJSON("/ObtenerTurno/ObtenerAgendaProfesional").done(function (data) {
+            for (var i = data.length - 1; i >= 0; i--) {
+                var turno = data[i];
+                turno.reservarTurno = function(){
+                    $(self).trigger('reservarTurno', [turno]);
+                };
+                self.turnosDisponibles.push(turno);
+            };                                  
+            
+            //Render del calendar
+            console.log("Render");
+            console.log($('#calendar'));
+            $('#calendar').fullCalendar({events:[]});                                
+        });          
+    };
+
+
+    var comprobanteVm = function(){
+        var self = this;
+        
+        self.imprimir = function(){
+            console.log("Imprimir");
+        };
+    };    
+
     var stepSeleccionProfesional = new step(1, 'Seleccion De Profesional', 'selectProfesional', new seleccionProfesionalesVm());
+    var stepSeleccionTurno = new step(2, 'Seleccion De Turno', 'selectTurno', new seleccionTurnoVm());
+    var stepComprobante = new step(3, 'Comprobante', 'selectComprobante', new comprobanteVm());
 
     var wizard = function () {
         var self = this;
         self.stepModels = ko.observableArray([
-                stepSeleccionProfesional
+                stepSeleccionProfesional,
+                stepSeleccionTurno,
+                stepComprobante
             ]);
 
         self.currentStep = ko.observable(self.stepModels()[0]);
+
+        self.profesionalSeleccionado = ko.observable(null);
+        self.turnoSeleccionado = ko.observable(null);
 
         self.getTemplate = function () {
             return self.currentStep().template;
         };
 
         self.reservarTurnoLibre = function (e, profesional) {
-            console.log("reservarTurnoLibre " + profesional.Id);
+            self.profesionalSeleccionado(profesional);
+            self.turnoSeleccionado(profesional.ProximoTurno);
+            self.currentStep(self.stepModels()[2]);
         };
         self.elegirTurnoEnAgenda = function (e, profesional) {
-            console.log("elegirTurnoEnAgenda " + profesional.Id);
+            self.profesionalSeleccionado(profesional);
+            self.currentStep(self.stepModels()[1]);
+        };
+        self.reservarTurno = function (e, turno) {
+            self.turnoSeleccionado(turno);
+            self.currentStep(self.stepModels()[2]);
         };
 
         $(stepSeleccionProfesional.model()).bind('reservarTurnoLibre', self.reservarTurnoLibre);
         $(stepSeleccionProfesional.model()).bind('elegirTurnoEnAgenda', self.elegirTurnoEnAgenda);
+        
+        $(stepSeleccionTurno.model()).bind('reservarTurno', self.reservarTurno);
 
     };
 
