a38e4e08f7648fb6e1babe9d58ad40eeab4cdf10
Walter Poch <walter.poch@gmail.com>
2012-01-28 12:33:12 -0300

    Acomodado el sitio para la template nueva.

---
+++ Sicemed/Sicemed.Web/public/js/features/ObtenerTurno/mocks/BuscarProfesional.js
@@ -0,0 +1,20 @@
+[
+  {
+    "Id":213456,
+    "Foto": "/public/images/personal_128x128.png",
+    "Nombre": "Perez, Raúl",
+    "Especialidades":[
+      "Dermatologo", "Kinesiologo"
+    ],
+    "ProximoTurno": "2012-01-26T11:00:00"
+  },
+  {
+    "Id":5465,
+    "Foto": "/public/images/personal_128x128.png",
+    "Nombre":"Poch, Walter",
+    "Especialidades":[
+      "Clinico"
+    ],
+    "ProximoTurno": null
+  }
+]
