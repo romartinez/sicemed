b069817dc27a6f813547a2fdc4b2618a2fdd4a75
Walter Poch <walter.poch@gmail.com>
2012-02-01 17:40:48 -0300

    Agregué el google maps.

--- Sicemed/Sicemed.Web/public/js/app/app.js
+++ Sicemed/Sicemed.Web/public/js/app/app.js
@@ -3,45 +3,26 @@
 
 var app = (function ($, app) {
     app.initialize = function () {
-        //Menu
-        $(function () {
-            /**
-            * for each menu element, on mouseenter, 
-            * we enlarge the image, and show both sdt_active span and 
-            * sdt_wrap span. If the element has a sub menu (sdt_box),
-            * then we slide it - if the element is the last one in the menu
-            * we slide it to the left, otherwise to the right
-            */
-            $('#sdt_menu > li').bind('mouseenter', function () {
-                var $elem = $(this);
-                $elem.find('.sdt_wrap')
-					     .stop(true)
-						 .animate({ 'top': '30px' }, 500, 'easeOutBack')
-						 .andSelf()
-						 .find('.sdt_active')
-					     .stop(true)
-						 .animate({ 'height': '20px' }, 300, function () {
-						     var $sub_menu = $elem.find('.sdt_box');
-						     if ($sub_menu.length) {
-						         $sub_menu.show('blind', { direction: 'vertical' }, 100);
-						     }
-						 });
-            }).bind('mouseleave', function () {
-                var $elem = $(this);
-                var $sub_menu = $elem.find('.sdt_box');
-                if ($sub_menu.length)
-                    $sub_menu.animate({ 'left': '0px' }, 200, function () {
-                        $sub_menu.hide('blind', { direction: 'vertical' }, 100);
-                    });
+        //Google map
+        var myOptions = {
+            center: new google.maps.LatLng(-34.397, 150.644),
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("google-map"), myOptions);
 
-                $elem.find('.sdt_active')
-						 .stop(true)
-						 .animate({ 'height': '0px' }, 300)
-						 .andSelf()
-						 .find('.sdt_wrap')
-						 .stop(true)
-						 .animate({ 'top': '25px' }, 500);
-            });
+        $("#google-map-wrapper").dialog({
+            autoOpen: false,
+            width: 555,
+            height: 400,
+            modal: true,
+            resizeStop: function (event, ui) { google.maps.event.trigger(map, 'resize'); },
+            open: function (event, ui) { google.maps.event.trigger(map, 'resize'); }
+        });
+
+        $("#google-map-link").click(function () {
+            $("#google-map-wrapper").dialog("open");            
+            return false;
         });
     };
     return app;
