b5077cb959507670fd735bdb0129fa95302a78ba
walter.poch@gmail.com <walter.poch@gmail.com>
2012-06-02 21:25:29 -0300

    Ajax filters para mensajes.

---
+++ Src/Sicemed.Web/Infrastructure/Attributes/Filters/AjaxMessagesFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Sicemed.Web.Infrastructure.Controllers;
+using Sicemed.Web.Infrastructure.Helpers;
+
+namespace Sicemed.Web.Infrastructure.Attributes.Filters
+{
+    public class AjaxMessagesFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                if (filterContext.Controller.TempData.ContainsKey(BaseController.MESSAGES_KEY))
+                {
+                    var storedMessages = (List<ResponseMessage>)filterContext.Controller.TempData[BaseController.MESSAGES_KEY];
+                    filterContext.HttpContext.Response.AddHeader("X-ResponseMessages", Json.SerializeObject(storedMessages));
+                }
+            }
+        }
+    }
+}
