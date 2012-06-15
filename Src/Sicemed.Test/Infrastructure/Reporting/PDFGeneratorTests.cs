288a51bb3ce6dffbc6c62881dc0ec7d807e6473c
walter.poch@gmail.com <walter.poch@gmail.com>
2012-06-15 01:49:25 -0300

    Quite el ReportingServices, no se estaba usando.

--- Src/Sicemed.Test/Infrastructure/Reporting/PDFGeneratorTests.cs
+++ Src/Sicemed.Test/Infrastructure/Reporting/PDFGeneratorTests.cs
@@ -1,31 +1,31 @@
-﻿using System.IO;
-using NUnit.Framework;
-using Sicemed.Web.Infrastructure.Reporting;
+﻿//using System.IO;
+//using NUnit.Framework;
+//using Sicemed.Web.Infrastructure.Reporting;
 
-namespace Sicemed.Tests.Infrastructure.Reporting
-{
-    [TestFixture]
-    public class PDFGeneratorTests
-    {
-        [Test]
-        [Ignore("No se está usando el Reporting Services Local")]
-        public void CanCreateAnEmptyReport()
-        {
-            var g = new PDFGenerator();
-            var b = g.OnTheFlyRender(null, null);
+//namespace Sicemed.Tests.Infrastructure.Reporting
+//{
+//    [TestFixture]
+//    public class PDFGeneratorTests
+//    {
+//        [Test]
+//        [Ignore("No se está usando el Reporting Services Local")]
+//        public void CanCreateAnEmptyReport()
+//        {
+//            var g = new PDFGenerator();
+//            var b = g.OnTheFlyRender(null, null);
 
-            File.WriteAllBytes(@"C:\test.pdf", b);
-        }        
+//            File.WriteAllBytes(@"C:\test.pdf", b);
+//        }        
         
-        [Test]
-        [Ignore("No se está usando el Reporting Services Local")]
-        public void CanCreateReport()
-        {
-            var g = new PDFGenerator();
-            var b = g.OnTheFlyRender("WALTERPOCH", null);
+//        [Test]
+//        [Ignore("No se está usando el Reporting Services Local")]
+//        public void CanCreateReport()
+//        {
+//            var g = new PDFGenerator();
+//            var b = g.OnTheFlyRender("WALTERPOCH", null);
 
-            File.WriteAllBytes(@"C:\test2.pdf", b);
-        }
+//            File.WriteAllBytes(@"C:\test2.pdf", b);
+//        }
          
-    }
-}
+//    }
+//}
