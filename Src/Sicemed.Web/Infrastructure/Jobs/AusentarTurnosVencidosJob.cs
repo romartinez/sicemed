b85d199c70b56ac244847c058c5b032a0b954afd
Walter Poch <walter.poch@gmail.com>
2012-06-22 12:54:22 -0300

    Agregué loging.

--- Src/Sicemed.Web/Infrastructure/Jobs/AusentarTurnosVencidosJob.cs
+++ Src/Sicemed.Web/Infrastructure/Jobs/AusentarTurnosVencidosJob.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Practices.ServiceLocation;
 using Sicemed.Web.Infrastructure.Queries.Jobs;
 using WebBackgrounder;
+using log4net;
 
 namespace Sicemed.Web.Infrastructure.Jobs
 {
     public class AusentarTurnosVencidosJob : Job
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(AusentarTurnosVencidosJob));
+
         public AusentarTurnosVencidosJob() 
             : base("Ausentar Turnos Vencidos", TimeSpan.FromDays(1), TimeSpan.FromMinutes(5))
         {
@@ -20,12 +24,15 @@ namespace Sicemed.Web.Infrastructure.Jobs
 
         private static void Run()
         {
-            var turnos = ServiceLocator.Current.GetInstance<IObtenerTurnosAMarcarComoAusentadosQuery>().Execute();
+            if (Log.IsInfoEnabled) Log.Info("Running AusentarTurnosVencidosJob");
+            var turnos = ServiceLocator.Current.GetInstance<IObtenerTurnosAMarcarComoAusentadosQuery>().Execute().ToList();
 
             foreach (var turno in turnos)
             {
                 turno.MarcarAusente();
             }
+
+            if (Log.IsDebugEnabled) Log.DebugFormat("Marcados como ausentes {0} turnos.", turnos.Count);
         }
     }
 }
--- Src/Sicemed.Web/Infrastructure/Jobs/KeepAliveJob.cs
+++ Src/Sicemed.Web/Infrastructure/Jobs/KeepAliveJob.cs
@@ -2,12 +2,16 @@
 using System.Configuration;
 using System.Net;
 using System.Threading.Tasks;
+using Sicemed.Web.App_Start;
 using WebBackgrounder;
+using log4net;
 
 namespace Sicemed.Web.Infrastructure.Jobs
 {
     public class KeepAliveJob : Job
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(KeepAliveJob));
+
         public KeepAliveJob()
             : base("Keep Alive", TimeSpan.FromMinutes(1), TimeSpan.FromSeconds(30))
         {}
@@ -19,8 +23,11 @@ namespace Sicemed.Web.Infrastructure.Jobs
 
         private static void Run()
         {
+            if(Log.IsInfoEnabled) Log.Info("Running KeepAlive...");
             var url = ConfigurationManager.AppSettings["KeepAliveUrl"];
             if(string.IsNullOrWhiteSpace(url)) return;
+
+            if (Log.IsDebugEnabled) Log.DebugFormat("Pinging {0}", url);
 
             var client = new WebClient();
             client.DownloadStringAsync(new Uri(url));
