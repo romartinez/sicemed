2911cfa673441aa028edfb4e5390d6a858f31fd8
walter.poch@gmail.com <walter.poch@gmail.com>
2012-08-07 00:30:15 -0300

    Nuevo combo para búsqueda de personas.

---
+++ Src/Sicemed.Web/Controllers/BusquedaController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Sicemed.Web.Infrastructure.Controllers;
+using Sicemed.Web.Infrastructure.Queries.Busqueda;
+using Sicemed.Web.Models.Roles;
+
+namespace Sicemed.Web.Controllers
+{
+    public class BusquedaController : BaseController
+    {
+        public ActionResult Paciente(string filtro)
+        {
+            var query = QueryFactory.Create<IBusquedaPersonaQuery>();
+            query.Rol = typeof (Paciente);
+            query.Filtro = filtro;
+            return Json(query.Execute(), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Src/Sicemed.Web/Infrastructure/Queries/Busqueda/BusquedaPersonaQueryQuery.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using NHibernate;
+using NHibernate.Criterion;
+using Sicemed.Web.Models;
+using Sicemed.Web.Models.Roles;
+using Sicemed.Web.Models.ViewModels;
+
+namespace Sicemed.Web.Infrastructure.Queries.Busqueda
+{
+    public interface IBusquedaPersonaQuery : IQuery<IEnumerable<InfoViewModel>>
+    {
+        Type Rol { get; set; }
+        string Filtro { get; set; }
+    }
+
+    public class BusquedaPersonaQueryQuery : Query<IEnumerable<InfoViewModel>>, IBusquedaPersonaQuery
+    {
+        public Type Rol { get; set; }
+        public string Filtro { get; set; }
+
+        protected override IEnumerable<InfoViewModel> CoreExecute()
+        {
+            var session = SessionFactory.GetCurrentSession();
+
+            var query = session.QueryOver<Persona>();
+
+            if (!string.IsNullOrWhiteSpace(Filtro))
+            {
+                query.Where(
+                    Restrictions.On<Persona>(x => x.Nombre).IsInsensitiveLike(Filtro, MatchMode.Anywhere)
+                    || Restrictions.On<Persona>(x => x.SegundoNombre).IsInsensitiveLike(Filtro, MatchMode.Anywhere)
+                    || Restrictions.On<Persona>(x => x.Apellido).IsInsensitiveLike(Filtro, MatchMode.Anywhere)
+                    || Restrictions.Like(Projections.Cast(NHibernateUtil.String, Projections.Property<Persona>(x => x.Documento.Numero)), Filtro, MatchMode.Anywhere)
+                    );
+            }
+
+            if (Rol != null)
+            {
+                query.JoinQueryOver<Rol>(x => x.Roles).Where(x => x.GetType() == Rol);
+            }
+
+            var personas = query.Future();
+
+            return MappingEngine.Map<IEnumerable<InfoViewModel>>(personas);
+        }
+    }
+}
