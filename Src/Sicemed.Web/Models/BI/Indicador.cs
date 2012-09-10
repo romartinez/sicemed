2003a3a335dad942c119ed2f8e992882d8910205
walter.poch@gmail.com <walter.poch@gmail.com>
2012-09-10 08:41:45 -0300

    BI

--- Src/Sicemed.Web/Models/BI/Indicador.cs
+++ Src/Sicemed.Web/Models/BI/Indicador.cs
@@ -15,23 +15,12 @@ namespace Sicemed.Web.Models.BI
         public virtual string Descripcion { get; set; }
         [Required]
         public virtual TipoOperadorIndicador TipoOperador { get; set; }
-        //[Required]
-        //public virtual TipoIndicador TipoIndicador { get; set; }
 
         public virtual ISet<ObjetivoIndicador> Objetivos { get; set; }
 
         public Indicador ()
         {
             Objetivos = new HashedSet<ObjetivoIndicador>();
-        }
-
-        public virtual void Calcular()
-        {
-            
-        }
-        public virtual void Calcular(DateTime fecha)
-        {
-            
         }
     }
 }
