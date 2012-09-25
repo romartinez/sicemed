ba714bb2f25dbcb911136ae21f02ef855ac76064
Walter Poch <walter.poch@gmail.com>
2012-09-25 16:01:06 -0300

    Cambio de modelo.

--- Src/Sicemed.Web/Models/Caja/MovimientoFactura.cs
+++ Src/Sicemed.Web/Models/Caja/MovimientoFactura.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using Iesi.Collections.Generic;
 
 namespace Sicemed.Web.Models.Caja
 {
     public class MovimientoFactura : Entity
     {
         public virtual decimal Importe { get; set; }
-        public virtual Valor Valor { get; set; }
+        public virtual Factura Factura { get; set; }
         public virtual DateTime FechaCarga { get; set; }
+        public virtual ISet<Valor> Valores { get; set; }
     }
 }
--- Src/Sicemed.Web/Models/Proveedor.cs
+++ Src/Sicemed.Web/Models/Proveedor.cs
@@ -1,7 +1,13 @@
-﻿namespace Sicemed.Web.Models
+﻿using System.Collections.Generic;
+using Sicemed.Web.Models.Components;
+
+namespace Sicemed.Web.Models
 {
     public class Proveedor : Entity
     {
         public virtual string RazonSocial { get; set; }
+        public virtual Documento Documento { get; set; }
+        public virtual Domicilio Domicilio { get; set; }
+        public virtual IList<Telefono> Telefonos { get; set; }
     }
 }
