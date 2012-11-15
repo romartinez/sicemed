e9c772b72d9831176b802644d094dd97d169d2e1
Walter Poch <walter.poch@gmail.com>
2012-11-15 17:42:19 -0300

    Faltan unos detalles pero se encuentra andnado.

--- Src/Sicemed.Web/Infrastructure/Reports/FichaPacienteReporte.cs
+++ Src/Sicemed.Web/Infrastructure/Reports/FichaPacienteReporte.cs
@@ -1,62 +1,152 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using NHibernate.Criterion;
 using NHibernate.Transform;
-using Sicemed.Web.Infrastructure.Helpers;
 using Sicemed.Web.Models;
+using Sicemed.Web.Models.Components;
 using Sicemed.Web.Models.Enumerations;
 using Sicemed.Web.Models.Reports;
+using Sicemed.Web.Models.Roles;
 
 namespace Sicemed.Web.Infrastructure.Reports
 {
-    public interface ITurnosReporte : IReport<TurnosReportModel>
+    public interface IFichaPacienteReporte : IReport<FichaPacienteReportModel>
     {
-        DateTime Fecha { get; set; }
+        DateTime FechaDesde { get; set; }
+        DateTime FechaHasta { get; set; }
+        string Filtro { get; set; }
+        long? PacienteId { get; set; }
     }
 
-    public class TurnosReporte : ReporteBase<TurnosReportModel>, ITurnosReporte
+    public class FichaPacienteReporte : ReporteBase<FichaPacienteReportModel>, IFichaPacienteReporte
     {
-        public virtual DateTime Fecha { get; set; }
+        private Paciente _paciente;
+        private long? _pacienteId;
 
-        public override string Title { get { return string.Format("Turnos Del Día: {0:dd/MM/yyyy}", Fecha); } }
-
-        public override string Name { get { return "TurnosReporte"; } }        
-
-        public override IEnumerable<TurnosReportModel> Execute()
+        public DateTime FechaDesde { get; set; }
+        public DateTime FechaHasta { get; set; }
+        public string Filtro { get; set; }
+        public long? PacienteId
         {
-            var desde = Fecha.ToMidnigth();
-            var hasta = desde.AddDays(1).ToMidnigth();
-
-            var session = SessionFactory.GetCurrentSession();
-
-            var turnos = session.QueryOver<Turno>()
-                .Fetch(t => t.Consultorio).Eager
-                .Fetch(t => t.Profesional).Eager
-                .Fetch(t => t.Profesional.Persona).Eager
-                .Fetch(t => t.Paciente).Eager
-                .Fetch(t => t.Paciente.Persona).Eager
-                .Where(t => t.Estado != EstadoTurno.Cancelado)
-                .Where(t => t.FechaTurno >= desde)
-                .And(t => t.FechaTurno <= hasta)
-                .TransformUsing(Transformers.DistinctRootEntity)                
-                .OrderBy(t => t.FechaTurno).Asc
-                .List();
-
-            return turnos.Select(Convert);
+            get { return _pacienteId; }
+            set
+            {
+                _pacienteId = value;
+                _paciente = SessionFactory.GetCurrentSession().Load<Paciente>(value);
+            }
         }
 
-        private TurnosReportModel Convert(Turno turno)
+        public override string Title { get { return string.Format("Ficha Paciente \nFechas: {0:dd/MM/yyyy} hasta {1:dd/MM/yyyy} Filtro: {2}", FechaDesde, FechaHasta, string.IsNullOrWhiteSpace(Filtro) ? "*Sin Filtro*" : Filtro); } }
+
+        public override string Name { get { return "FichaPacienteReporte"; } }
+
+        public override IEnumerable<FichaPacienteReportModel> Execute()
         {
-            return new TurnosReportModel
+            var session = SessionFactory.GetCurrentSession();
+            var query = session.QueryOver<Turno>()
+                .Fetch(t => t.Paciente).Eager
+                .Fetch(t => t.Paciente.Persona).Eager
+                .Fetch(t => t.Profesional).Eager
+                .Fetch(t => t.Profesional.Persona).Eager
+                .Fetch(t => t.Consultorio).Eager
+                .Fetch(t => t.Especialidad).Eager
+                .Where(t => t.FechaTurno >= FechaDesde)
+                .Where(t => t.FechaTurno <= FechaHasta)
+                .Where(t => t.Paciente == _paciente)
+                //Solo los que estan atendidos
+                .Where(t => t.Estado == EstadoTurno.Atendido)
+                .OrderBy(t => t.FechaTurno).Desc;
+
+            if (!string.IsNullOrWhiteSpace(Filtro))
             {
-                FechaTurnos = turno.FechaTurno.ToMidnigth(),
-                Profesional = turno.Profesional.Persona.NombreCompleto,
-                Paciente = turno.Paciente.Persona.NombreCompleto,
-                Consultorio = turno.Consultorio != null ? turno.Consultorio.Nombre : string.Empty,
-                Especialidad = turno.Especialidad != null ? turno.Especialidad.Nombre : string.Empty,
+                Models.Roles.Profesional profesional = null;
+                Persona persona = null;
+
+                query.Left.JoinAlias(x => x.Profesional, () => profesional)
+                    .Left.JoinAlias(x => profesional.Persona, () => persona)
+                .Where(
+                    Restrictions.On<Turno>(x => x.Nota).IsInsensitiveLike(Filtro, MatchMode.Anywhere)
+                        || Restrictions.On(() => persona.Nombre).IsInsensitiveLike(Filtro, MatchMode.Start)
+                        || Restrictions.On(() => persona.SegundoNombre).IsInsensitiveLike(Filtro, MatchMode.Start)
+                        || Restrictions.On(() => persona.Apellido).IsInsensitiveLike(Filtro, MatchMode.Start)
+                );
+            }
+
+            query.JoinQueryOver<CambioEstadoTurno>(x => x.CambiosDeEstado).JoinQueryOver(x => x.Responsable)
+                .TransformUsing(new DistinctRootEntityResultTransformer());
+
+            var turnos = query.List();
+
+            if (turnos.Any()) return turnos.Select(Convert);
+
+            return new List<FichaPacienteReportModel> { AppendPacienteInfo(new FichaPacienteReportModel()) };
+        }
+
+        private FichaPacienteReportModel Convert(Turno turno)
+        {
+            var model = new FichaPacienteReportModel
+            {
                 FechaTurno = turno.FechaTurno,
-                FechaTurnoFin = turno.FechaTurnoFinal
+                Profesional = turno.Profesional.Persona.NombreCompleto,
+                Consultorio = turno.Consultorio != null ? turno.Consultorio.Nombre : string.Empty,
+                Especialidad = turno.Especialidad != null ? turno.Especialidad.Descripcion : string.Empty,
+                Nota = turno.Nota
             };
+
+            return AppendPacienteInfo(model);
+        }
+
+        private FichaPacienteReportModel AppendPacienteInfo(FichaPacienteReportModel model)
+        {
+            model.NombrePaciente = _paciente.Persona.NombreCompleto;
+            model.Email = _paciente.Persona.Membership.Email;
+
+            if (_paciente.Persona.Documento != null)
+            {
+                model.DocumentoTipo = _paciente.Persona.Documento.TipoDocumento.DisplayName;
+                model.DocumentoNumero = _paciente.Persona.Documento.Numero.ToString();
+            }
+
+            if (_paciente.Persona.Domicilio != null)
+            {
+                model.Domicilio = _paciente.Persona.Domicilio.Direccion;
+                if (_paciente.Persona.Domicilio.Localidad != null)
+                {
+                    model.Domicilio = string.Format("{0}, {1} ({2}), {3}", model.Domicilio,
+                                                    _paciente.Persona.Domicilio.Localidad.Nombre,
+                                                    _paciente.Persona.Domicilio.Localidad.CodigoPostal,
+                                                    _paciente.Persona.Domicilio.Localidad.Provincia.Nombre);
+                }
+            }
+
+            if (_paciente.Persona.Telefono != null)
+            {
+                model.Telefono = string.Format("({0}) - {1}", _paciente.Persona.Telefono.Prefijo,
+                                               _paciente.Persona.Telefono.Numero);
+            }
+
+            if (_paciente.Persona.FechaNacimiento.HasValue)
+                model.FechaNacimiento = _paciente.Persona.FechaNacimiento.Value.ToShortDateString();
+
+            if (_paciente.Persona.Edad.HasValue)
+                model.Edad = _paciente.Persona.Edad.Value.ToString();
+
+            if (_paciente.Persona.Peso.HasValue)
+                model.Peso = _paciente.Persona.Peso.Value.ToString();
+
+            if (_paciente.Persona.Altura.HasValue)
+                model.Altura = _paciente.Persona.Altura.Value.ToString();
+
+            model.NumeroAfiliado = _paciente.NumeroAfiliado;
+
+            if (_paciente.Plan != null)
+            {
+                model.Plan = _paciente.Plan.Nombre;
+                model.ObraSocial = _paciente.Plan.ObraSocial.RazonSocial;
+            }
+
+            return model;
         }
     }
 }
--- Src/Sicemed.Web/Infrastructure/Reports/TurnosReporte.cs
+++ Src/Sicemed.Web/Infrastructure/Reports/TurnosReporte.cs
@@ -1,141 +1,62 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using NHibernate.Criterion;
 using NHibernate.Transform;
+using Sicemed.Web.Infrastructure.Helpers;
 using Sicemed.Web.Models;
-using Sicemed.Web.Models.Components;
+using Sicemed.Web.Models.Enumerations;
 using Sicemed.Web.Models.Reports;
-using Sicemed.Web.Models.Roles;
 
 namespace Sicemed.Web.Infrastructure.Reports
 {
-    public interface IFichaPacienteReporte : IReport<FichaPacienteReportModel>
+    public interface ITurnosReporte : IReport<TurnosReportModel>
     {
-        DateTime FechaDesde { get; set; }
-        DateTime FechaHasta { get; set; }
-        string Filtro { get; set; }
-        long? PacienteId { get; set; }
+        DateTime Fecha { get; set; }
     }
 
-    public class FichaPacienteReporte : ReporteBase<FichaPacienteReportModel>, IFichaPacienteReporte
+    public class TurnosReporte : ReporteBase<TurnosReportModel>, ITurnosReporte
     {
-        private Paciente _paciente;
-        private long? _pacienteId;
+        public virtual DateTime Fecha { get; set; }
 
-        public DateTime FechaDesde { get; set; }
-        public DateTime FechaHasta { get; set; }
-        public string Filtro { get; set; }
-        public long? PacienteId
+        public override string Title { get { return string.Format("Turnos Del Día: {0:dd/MM/yyyy}", Fecha); } }
+
+        public override string Name { get { return "TurnosReporte"; } }        
+
+        public override IEnumerable<TurnosReportModel> Execute()
         {
-            get { return _pacienteId; }
-            set { 
-                _pacienteId = value;
-                _paciente = SessionFactory.GetCurrentSession().Load<Paciente>(value);
-            }
-        }
+            var desde = Fecha.ToMidnigth();
+            var hasta = desde.AddDays(1).ToMidnigth();
 
-        public override string Title { get { return string.Format("Ficha Paciente: {0}", _paciente.Persona.NombreCompleto); } }
+            var session = SessionFactory.GetCurrentSession();
 
-        public override string Name { get { return "FichaPacienteReporte"; } }
-
-        public override IEnumerable<FichaPacienteReportModel> Execute()
-        {
-            var session = SessionFactory.GetCurrentSession();            
-            var query = session.QueryOver<Turno>()
-                .Fetch(t => t.Paciente).Eager
-                .Fetch(t => t.Paciente.Persona).Eager
+            var turnos = session.QueryOver<Turno>()
+                .Fetch(t => t.Consultorio).Eager
                 .Fetch(t => t.Profesional).Eager
                 .Fetch(t => t.Profesional.Persona).Eager
-                .Fetch(t => t.Consultorio).Eager
-                .Fetch(t => t.Especialidad).Eager
-                .Where(t => t.FechaTurno >= FechaDesde)
-                .Where(t => t.FechaTurno <= FechaHasta)
-                .Where(t => t.Paciente == _paciente)
-                .OrderBy(t => t.FechaTurno).Desc;
-
-            if (!string.IsNullOrWhiteSpace(Filtro))
-            {
-                Models.Roles.Profesional profesional = null;
-                Persona persona = null;
-
-                query.Left.JoinAlias(x => x.Profesional, () => profesional)
-                    .Left.JoinAlias(x => profesional.Persona, () => persona)
-                .Where(
-                    Restrictions.On<Turno>(x => x.Nota).IsInsensitiveLike(Filtro, MatchMode.Anywhere)
-                        || Restrictions.On(() => persona.Nombre).IsInsensitiveLike(Filtro, MatchMode.Start)
-                        || Restrictions.On(() => persona.SegundoNombre).IsInsensitiveLike(Filtro, MatchMode.Start)
-                        || Restrictions.On(() => persona.Apellido).IsInsensitiveLike(Filtro, MatchMode.Start)
-                );
-            }
-
-            query.JoinQueryOver<CambioEstadoTurno>(x => x.CambiosDeEstado).JoinQueryOver(x => x.Responsable)
-                .TransformUsing(new DistinctRootEntityResultTransformer());
-
-            var turnos = query.Future();
+                .Fetch(t => t.Paciente).Eager
+                .Fetch(t => t.Paciente.Persona).Eager
+                .Where(t => t.Estado != EstadoTurno.Cancelado)
+                .Where(t => t.FechaTurno >= desde)
+                .And(t => t.FechaTurno <= hasta)
+                .TransformUsing(Transformers.DistinctRootEntity)                
+                .OrderBy(t => t.FechaTurno).Asc
+                .List();
 
             return turnos.Select(Convert);
         }
 
-        private FichaPacienteReportModel Convert(Turno turno)
+        private TurnosReportModel Convert(Turno turno)
         {
-            var model = new FichaPacienteReportModel
+            return new TurnosReportModel
             {
-                NombrePaciente = turno.Paciente.Persona.NombreCompleto,
-                Email = turno.Paciente.Persona.Membership.Email,
-
-                FechaTurno = turno.FechaTurno,
+                FechaTurnos = turno.FechaTurno.ToMidnigth(),
                 Profesional = turno.Profesional.Persona.NombreCompleto,
+                Paciente = turno.Paciente.Persona.NombreCompleto,
                 Consultorio = turno.Consultorio != null ? turno.Consultorio.Nombre : string.Empty,
-                Especialidad = turno.Especialidad != null ? turno.Especialidad.Descripcion : string.Empty,
-                Nota = turno.Nota
+                Especialidad = turno.Especialidad != null ? turno.Especialidad.Nombre : string.Empty,
+                FechaTurno = turno.FechaTurno,
+                FechaTurnoFin = turno.FechaTurnoFinal
             };
-
-            if (turno.Paciente.Persona.Documento != null)
-            {
-                model.DocumentoTipo = turno.Paciente.Persona.Documento.TipoDocumento.DisplayName;
-                model.DocumentoNumero = turno.Paciente.Persona.Documento.Numero.ToString();
-            }
-
-            if (turno.Paciente.Persona.Domicilio != null)
-            {
-                model.Domicilio = turno.Paciente.Persona.Domicilio.Direccion;
-                if (turno.Paciente.Persona.Domicilio.Localidad != null)
-                {
-                    model.Domicilio = string.Format("{0}, {1} ({2}), {3}", model.Domicilio,
-                                                    turno.Paciente.Persona.Domicilio.Localidad.Nombre,
-                                                    turno.Paciente.Persona.Domicilio.Localidad.CodigoPostal,
-                                                    turno.Paciente.Persona.Domicilio.Localidad.Provincia.Nombre);
-                }
-            }
-
-            if (turno.Paciente.Persona.Telefono != null)
-            {
-                model.Telefono = string.Format("({0}) - {1}", turno.Paciente.Persona.Telefono.Prefijo,
-                                               turno.Paciente.Persona.Telefono.Numero);
-            }
-
-            if (turno.Paciente.Persona.FechaNacimiento.HasValue)
-                model.FechaNacimiento = turno.Paciente.Persona.FechaNacimiento.Value.ToShortDateString();
-
-            if (turno.Paciente.Persona.Edad.HasValue)
-                model.Edad = turno.Paciente.Persona.Edad.Value.ToString();
-
-            if (turno.Paciente.Persona.Peso.HasValue)
-                model.Peso = turno.Paciente.Persona.Peso.Value.ToString();
-
-            if (turno.Paciente.Persona.Altura.HasValue)
-                model.Altura = turno.Paciente.Persona.Altura.Value.ToString();
-
-            model.NumeroAfiliado = turno.Paciente.NumeroAfiliado;
-
-            if (turno.Paciente.Plan != null)
-            {
-                model.Plan = turno.Paciente.Plan.Nombre;
-                model.ObraSocial = turno.Paciente.Plan.ObraSocial.RazonSocial;
-            }
-
-            return model;
         }
     }
 }
